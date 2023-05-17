06e8b7c26d1fef06cf560bc59bf67219ca05f26c
soobinLee <sooblair84@gmail,com>
2023-05-17 18:32:03 +0900

    create user_addmin pannel and adding filter

--- houses/admin.py
+++ houses/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import House
 
+
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    pass
-
-
+    fields = ("name", "address", ("price_per_night", "pets_allowed"))
+    list_display = ["name", "price_per_night", "address", "pets_allowed"]
+    list_filter = ["price_per_night", "pets_allowed"]
+    search_fields = ["address"]
