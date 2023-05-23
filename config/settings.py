351fcaf1cf63751607163a9848184ca0b72b59d2
soobinLee <sooblair84@gmail,com>
2023-05-23 17:51:37 +0900

    Media migrations

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ CUSTOM_APPS = [
     "reviews.apps.ReviewsConfig",
     "wishlists.apps.WishlistsConfig",
     "bookings.apps.BookingsConfig",
+    "medias.apps.MediasConfig",
 ]
 SYSTEM_APPS = [
     "django.contrib.admin",
