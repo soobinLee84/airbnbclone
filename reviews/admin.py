5078e31640cb31e2b43a2ca6d93b62f231e5f73c
soobinLee <sooblair84@gmail,com>
2023-05-25 18:49:41 +0900

    Admin Custom Filter

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,10 +2,35 @@ from django.contrib import admin
 from .models import Reiview
 
 
+class WorldFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("bad", "Bad"),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word == "good":
+            return reviews.filter(rating__gte=3)
+        elif word == "bad":
+            return reviews.filter(rating__lt=3)
+        else:
+            return reviews
+
+
 @admin.register(Reiview)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
         "__str__",
         "payload",
     )
-    list_filter = ("rating",)
+    list_filter = (
+        WorldFilter,
+        "rating",
+        "user__is_host",
+        "room__category",
+    )
